e3b597a88dac3e6bfe63773a9872404e99d75a4b
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2024-08-24 23:47:43 +0200

    Use new `Session`

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from requests import get, Session, session as requests_session
+from requests import get, Session
 
 from os import getenv
 from time import sleep
@@ -23,7 +23,7 @@ def sep(text: str, *, n: int = 4, char: str = '*') -> None:
     print('\n{0} {1} {0}'.format(n * char, text))
 
 
-def is_gluetun_ready() -> bool:
+def is_gluetun_ready(s: Session) -> bool:
     res = get(f'{GLUETUN_URL}/v1/openvpn/status')
     if (code := res.status_code) == 200:
         if res.json().get('status') == 'running':
@@ -33,25 +33,27 @@ def is_gluetun_ready() -> bool:
     sleep(5.0)
 
 
-def get_assigned_port() -> int:
+def get_assigned_port(s: Session) -> int:
     res = get(f'{GLUETUN_URL}/v1/openvpn/portforwarded')
     if (code := res.status_code) != 200:
         raise RuntimeError(f'Error {code}: {res.text}')
     return int(res.json().get('port'))
 
 
-def login_to_qbittorrent(session: Session) -> None:
-    res = session.post(
+def login_to_qbittorrent(s: Session) -> None:
+    res = s.post(
         url=f'{QBITTORRENT_URL}/api/v2/auth/login',
         data=f'username={QBITTORRENT_USER}&password={QBITTORRENT_PASSWORD}',
         headers=DEFAULT_QBT_HEADERS
     )
     if (code := res.status_code) != 200:
         raise RuntimeError(f'Error {code}: {res.text}')
+    if not res.cookies.get('SID'):
+        raise RuntimeError('Can\'t login (wrong password?)')
 
 
-def update_qbittorrent_port(session: Session, port: int) -> None:
-    res = session.post(
+def update_qbittorrent_port(s: Session, port: int) -> None:
+    res = s.post(
         url=f'{QBITTORRENT_URL}/api/v2/app/setPreferences',
         headers=DEFAULT_QBT_HEADERS,
         data='json={"listen_port":' + str(port) + '}'
@@ -73,52 +75,51 @@ def verify_qbittorrent_port(session: Session, port: int) -> None:
 
 
 def main():
-    sep('gluetun')
-
-    # Wait for gluetun
+    # gluetun
     try:
-        while not is_gluetun_ready():
-            print('Waiting for gluetun...')
-    except RuntimeError:
-        return
-    print('Gluetun is running')
+        with Session() as s:
+            sep('gluetun')
 
-    # Get assigned port
+            # Wait for gluetun
+            while not is_gluetun_ready(s):
+                print('Waiting for gluetun...')
+            print('Gluetun is running')
+
+            # Get assigned port
+            port = get_assigned_port(s)
+            print(f'Assigned port: {port}')
+    except Exception as e:
+        raise e
+    finally:
+        s.close()
+
+    # qBittorrent
     try:
-        port = get_assigned_port()
-    except RuntimeError:
-        return
-    print(f'Assigned port: {port}')
+        with Session() as s:
+            sep('qBittorrent')
 
-    # Use session to save cookies
-    qbt_session = requests_session()
+            # Login to qBittorent
+            print('Trying to login to qBittorrent...')
+            login_to_qbittorrent(s)
+            print('Logged in')
 
-    sep('qBittorrent')
+            # Update listening port
+            print('Trying to update listening port...')
+            update_qbittorrent_port(s, port)
+            print('Port updated')
 
-    # Login to qBittorent
-    print('Trying to login to qBittorrent...')
-    try:
-        login_to_qbittorrent(qbt_session)
-    except RuntimeError:
-        return
-    print('Logged in')
+            # Verify if port has been changed (for my sanity)
+            print('Verifying port...')
+            verify_qbittorrent_port(s, port)
+            print('Port verified')
 
-    # Update listening port
-    print('Trying to update listening port...')
-    update_qbittorrent_port(qbt_session, port)
-    print('Port updated')
-
-    # Verify if port has been changed (for my sanity)
-    print('Verifying port...')
-    try:
-        verify_qbittorrent_port(qbt_session, port)
-    except RuntimeError:
-        return
-    print('Port verified')
-
-    sep('Done')
-    print(f'Next run in {int(TIMEOUT)} seconds...')
-    sleep(TIMEOUT)
+            sep('Done')
+            print(f'Next run in {int(TIMEOUT)} seconds...')
+            sleep(TIMEOUT)
+    except Exception as e:
+        raise e
+    finally:
+        s.close()
 
 
 if __name__ == '__main__':
