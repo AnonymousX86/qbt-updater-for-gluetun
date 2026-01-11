e444705da49957a60a694fd23667234fd22cfdc4
AnonymousX86 <jakub.suchenek.25@gmail.com>
2026-01-11 01:37:47 +0100

    Fix #1

--- app/gluetun.py
+++ app/gluetun.py
@@ -10,7 +10,7 @@ BASE_URL = settings.gluetun.url
 
 
 def is_gluetun_ready(s: Session) -> bool:
-    res = s.get(f'{BASE_URL}/v1/openvpn/status')
+    res = s.get(f'{BASE_URL}/v1/vpn/status')
     if (code := res.status_code) == 200:
         if res.json().get('status') == 'running':
             return True
@@ -28,7 +28,7 @@ def wait_for_gluetun(s: Session) -> None:
 
 
 def get_assigned_port(s: Session) -> int:
-    res = s.get(f'{BASE_URL}/v1/openvpn/portforwarded')
+    res = s.get(f'{BASE_URL}/v1/portforward')
     if (code := res.status_code) != 200:
         raise RuntimeError(f'Error {code}: {res.text}')
     return int(res.json().get('port'))
